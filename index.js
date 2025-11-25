46397c07820f3674fbdd41ef943ed30de726e84b
MirajMalik <mirajmalik333@gmail.com>
2025-11-25 18:35:10 +0600

    full CRUD operation using MONGODB And Express

--- index.js
+++ index.js
@@ -59,33 +59,36 @@ app.get('/',(req,res)=>{
     res.send("We are in the home page");
 });
 
+
+// CRUD Operation
+
 // POST: /products -> create a product
-// app.post("/products", async (req,res) => {
-//     try{
-//         // get data from req body
-//         // const title = req.body.title;
-//         // const price = req.body.price;
-//         // const description = req.body.description;
+app.post("/products", async (req,res) => {
+    try{
+        // get data from req body
+        // const title = req.body.title;
+        // const price = req.body.price;
+        // const description = req.body.description;
 
-//         //const{title,price,description} = req.body;
+        //const{title,price,description} = req.body;
 
-//         const newProduct =  new Product({   // here product is the model name
-//             title : req.body.title,
-//             price : req.body.price,
-//             description : req.body.description,
-//         });
+        const newProduct =  new Product({   // here product is the model name
+            title : req.body.title,
+            price : req.body.price,
+            description : req.body.description,
+        });
 
-//         const productData = await newProduct.save();
+        const productData = await newProduct.save();
 
-//         res.status(201).send(productData);
+        res.status(201).send(productData);
 
 
 
-//     }catch(error){
-//         res.status(500).send({message : error.message});
-//     }
+    }catch(error){
+        res.status(500).send({message : error.message});
+    }
 
-// });
+});
 
 
 // // GET: /products -> Return all the products
@@ -109,93 +112,93 @@ app.get('/products', async (req,res) => {
 
 
 // GET: /products/:id -> Return a specific product
-// app.get('/products/:id', async (req,res) => {
-//     try{
-//          const id = req.params.id;
-//          //const product = await product.find({_id: id});   // modelname.find() to find the products from db.Find returns a array.
-//          const product = await Product.findOne({_id: id});  // findOne returns object.Returns everything
-//         //  const product = await product.findOne({_id: id}).select({
-//         //     title: 1,
-//         //     _id : 0
-//         //  });                   // for specific things we use select.1 means true .Here only the title will be displayed.0 means dont display.
+app.get('/products/:id', async (req,res) => {
+    try{
+         const id = req.params.id;
+         //const product = await product.find({_id: id});   // modelname.find() to find the products from db.Find returns a array.
+         const product = await Product.findOne({_id: id});  // findOne returns object.Returns everything
+        //  const product = await product.findOne({_id: id}).select({
+        //     title: 1,
+        //     _id : 0
+        //  });                   // for specific things we use select.1 means true .Here only the title will be displayed.0 means dont display.
 
-//         //  res.send(product);
+        //  res.send(product);
 
 
-//          if(product){
-//             res.status(200).send(product);
-//          }
-//          else{
-//              res.status(404).send({
-//                 message : "product not found",
-//              });
-//          }
-//     }catch(error){
-//         res.status(500).send({message : error.message});
-//     }
+         if(product){
+            res.status(200).send(product);
+         }
+         else{
+             res.status(404).send({
+                message : "product not found",
+             });
+         }
+    }catch(error){
+        res.status(500).send({message : error.message});
+    }
 
     
-// });
+});
 
 
 
 // GET: /products -> Return all the products using query operator
-// app.get('/products', async (req,res) => {
-//     try{
-//          const price = req.query.price;
-//          let products;
-//          //const products = await product.find({price : {$eq : 14000}});  // lt = less than ,gt = greater than , eq = equal ,ne = not equal ,gte,lte,in[array of values]
-//          //for user input price
-//          if(price){
-//               products = await product.find({price : {$gt : price}});
-//          }else{
-//               products = await product.find();
-//          }
+app.get('/products', async (req,res) => {
+    try{
+         const price = req.query.price;
+         let products;
+         //const products = await product.find({price : {$eq : 14000}});  // lt = less than ,gt = greater than , eq = equal ,ne = not equal ,gte,lte,in[array of values]
+         //for user input price
+         if(price){
+              products = await product.find({price : {$gt : price}});
+         }else{
+              products = await product.find();
+         }
          
 
-//          if(products){
-//             res.status(200).send(products);
-//          }
-//          else{
-//              res.status(404).send({
-//                 message : "products not found",
-//              });
-//          }
-//     }catch(error){
-//         res.status(500).send({message : error.message});
-//     }
+         if(products){
+            res.status(200).send(products);
+         }
+         else{
+             res.status(404).send({
+                message : "products not found",
+             });
+         }
+    }catch(error){
+        res.status(500).send({message : error.message});
+    }
 
     
-// });
+});
 
 
 //DELETE: /products/:id => delete a product based on id
 
-// app.delete('/products/:id',async (req,res) => {
-//     try{
-//         const id = req.params.id;
-//         const deletedProduct = await Product.findByIdAndDelete({_id : id});
-//         if(deletedProduct){
-//             res.status(200).send({
-//                 success : true,
-//                 message : "product deleted",
-//                 data : product,
+app.delete('/products/:id',async (req,res) => {
+    try{
+        const id = req.params.id;
+        const deletedProduct = await Product.findByIdAndDelete({_id : id});
+        if(deletedProduct){
+            res.status(200).send({
+                success : true,
+                message : "product deleted",
+                data : product,
 
-//             });
-//         }else{
-//             res.status(404).send({
-//                 success : false,
-//                 message : "product can not be deleted",
+            });
+        }else{
+            res.status(404).send({
+                success : false,
+                message : "product can not be deleted",
 
-//             });
-//         }
+            });
+        }
 
 
-//     }catch(error){
-//         console.log("Error Occured");
-//          res.status(500).send({message : error.message});
-//      }
-// });
+    }catch(error){
+        console.log("Error Occured");
+         res.status(500).send({message : error.message});
+     }
+});
 
 
 // update a product by id
